f0beb26013b6a5d8e59cb411b98d006b95e34c06
r3rivera <r.ryan.rivera@outlook.com>
2022-10-27 14:48:58 -0500

    Initial

---
+++ src/main/java/com/ryanrivera/app/frontdesk/OnlineFrontdeskApplication.java
@@ -0,0 +1,13 @@
+package com.ryanrivera.app.frontdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFrontdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFrontdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanrivera/app/frontdesk/OnlineFrontdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanrivera.app.frontdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFrontdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
